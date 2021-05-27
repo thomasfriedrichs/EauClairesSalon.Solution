7a060f0a82de52142d814a51ead82d285fcf9896
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-05-26 22:17:43 -0700

    add clients controller

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -33,38 +33,38 @@ namespace HairSalon.Controllers
       return RedirectToAction("Index");
     }
 
-    // public ActionResult Details(int id)
-    // {
-    //   Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
-    //   return View(thisStylist);
-    // }
-    // public ActionResult Edit(int id)
-    // {
-    //   Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
-    //   return View(thisStylist);
-    // }
+    public ActionResult Details(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+    public ActionResult Edit(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
 
-    // [HttpPost]
-    // public ActionResult Edit(Stylist stylist)
-    // {
-    //   _db.Entry(stylist).State = EntityState.Modified;
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Edit(Stylist stylist)
+    {
+      _db.Entry(stylist).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // public ActionResult Delete(int id)
-    // {
-    //   Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
-    //   return View(thisStylist);
-    // }
+    public ActionResult Delete(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
 
-    // [HttpPost, ActionName("Delete")]
-    // public ActionResult DeleteConfirmed(int id)
-    // {
-    //   Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
-    //   _db.Stylists.Remove(thisStylist);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      _db.Stylists.Remove(thisStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   } 
 }
